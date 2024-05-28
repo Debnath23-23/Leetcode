5d37fc16475cfd89d3899690eca46a665e156038
Debnath23-23 <150770621+Debnath23-23@users.noreply.github.com>
2024-05-28 12:52:47 +0530

    Time: 8 ms (97.76%), Space: 26.5 MB (99.08%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int n=nums.size();
+        int count=0;
+        int maj=NULL;
+        //boyer-moore voting algorithm
+        for(int i=0; i<n; i++){
+            if(nums[i]==maj){
+                count++;
+            }
+            else if(count==0){
+                maj=nums[i];
+                count=1;
+            }
+            else{
+                count--;
+            }
+
+        }
+        return maj;
+        
+    }
+};
