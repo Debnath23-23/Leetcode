70efeb3b5d9bc83db25bb721b8898b186d6e98d2
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2024-05-29 22:40:23 +0530

    Time: 11 ms (90.03%), Space: 16.9 MB (59.30%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+        ListNode* MergeSortTwoList(ListNode* l1,ListNode* l2){
+            if(l1==NULL)
+            return l2;
+            if(l2==NULL)
+            return l1;
+
+            if(l1->val<=l2->val){
+                l1->next=MergeSortTwoList(l1->next,l2);
+                return l1;
+            }else{
+                l2->next=MergeSortTwoList(l1,l2->next);
+                return l2;
+            }
+            return NULL;
+        }
+           ListNode* partitionAndMerge(int start,int end, vector<ListNode*>& lists){
+            if(start>end)
+            return NULL;
+
+            if(start==end)
+            return lists[start];
+
+            int mid= start+(end-start)/2;
+            ListNode* l1= partitionAndMerge(start, mid, lists);
+            ListNode* l2= partitionAndMerge(mid+1, end, lists);
+             
+             return  MergeSortTwoList(l1, l2);
+
+           }
+               ListNode* mergeKLists(vector<ListNode*>& lists) {
+            int k= lists.size();
+            if(k==0)return NULL;
+            return partitionAndMerge(0, k-1 ,lists);
+
+
+
+    }
+};
+
