f7b1ca4ef16779e74663a0e6dec5dab4d49515c7
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2024-06-10 22:28:55 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (44.04%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted(heights.begin(),heights.end());
+        sort(heights.begin(),heights.end());
+
+        int ans=0;
+        for(int i=0; i<heights.size(); i++){
+            if(heights[i]!=sorted[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
