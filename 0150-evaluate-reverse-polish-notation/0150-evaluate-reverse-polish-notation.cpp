0fc2f8f6f35ada9d09beca4899f1adf9717fc190
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2024-09-13 07:55:50 +0530

    Time: 8 ms (61.40%), Space: 15.9 MB (20.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-int operate(int a, int b, string tokens){
-    if(tokens=="+"){
-        return a+b;
-    }
-    else if(tokens=="-"){
-        return a-b;
-    }
-      else if(tokens=="*"){
-        return (long)a*(long)b;
-    }
-      else if(tokens=="/"){
-        return a/b;
-    }
-    return -1;
-}
+ 
 
     int evalRPN(vector<string>& tokens) {
+        unordered_map<string, function<int(int, int)>> mp={
+            
+                {"+",[](int a,int b){return a+b;}},
+                {"-",[](int a,int b){return a-b;}},
+                {"*",[](int a,int b){return (long)a*(long)b;}},
+                {"/",[](int a,int b){return a/b;}}
+
+            
+        };
         stack <int> st;
         for(string & tokens : tokens){
             if(tokens=="+" || tokens=="-" || tokens=="*" || tokens=="/" ){
@@ -26,7 +21,8 @@ int operate(int a, int b, string tokens){
                 int a=st.top();
                 st.pop();
 
-                int result=operate(a,b,tokens);        //mp[token],(a,b);
+                
+                int result=mp[tokens](a,b);        //mp[token],(a,b);
                 st.push(result);
             }else{
                 st.push(stoi(tokens));
