45c4d203bbd805fb1a4577bcd363bad4c32ed8e6
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2025-02-12 00:00:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (50.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        for(int j=0; j<nums.size(); j++){
-            if(nums[j]!=0){
-                swap(nums[j],nums[i]);
-                i++;
+        int n=nums.size();
+        int insertPosition=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                nums[insertPosition]=nums[i];
+                insertPosition++;
             }
         }
-        return;
+        while(insertPosition<n){
+            nums[insertPosition++]=0;
+        }
     }
 };
-
-
