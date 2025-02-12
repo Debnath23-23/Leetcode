bfba9b654fd5bcc1191eea2061260225be9993ba
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2025-02-12 08:17:53 +0530

    Time: 1 ms (46.44%), Space: 62.7 MB (99.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n= height.size();
+
+    // Brute force
+        // int mostWater=0;
+        // int n=height.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         int width= j-i;
+        //         int heightt=min(height[i],height[j]);
+        //         int area=width*heightt;
+        //         mostWater=max(mostWater, area);
+        //     }
+        // }
+        // return mostWater;
+
+        // optimize code
+        int n=height.size();
         int i=0;
         int j=n-1;
-        int mostWater=0;
+        int maxWater=0;
         while(i<j){
             int width=j-i;
-            int heights=min(height[i],height[j]);
-            int area= width*heights;
-            mostWater=max(mostWater,area);
-
-            if(height[i]>height[j]){
-                j--;
-            }else{
-                i++;
-            }
+            int dist=min(height[i],height[j]);
+            int area=width*dist;
+            maxWater=max(area,maxWater);
+            height[i]<height[j]?i++:j--;
         }
-        return mostWater;
+        return maxWater;
     }
 };
