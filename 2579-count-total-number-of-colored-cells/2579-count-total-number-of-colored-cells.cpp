19b74920f15b207ae83ec167a30b0f5c59bf93ca
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2025-03-05 11:56:49 +0530

    Time: 3 ms (20.68%), Space: 7.7 MB (99.69%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        return (1+2* (long long)(n-1)*n);
+        int t=1;
+        long long cells=1;
+        while(t<=n){
+            cells+=4*(t-1);
+            t++;
+        }
+        return cells;
     }
 };
