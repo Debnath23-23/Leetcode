7d9270bbcaada51aec671c746750adbf3c5236a7
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2026-01-28 21:45:01 +0530

    Time: 38 ms (86.48%) | Memory: 53.6 MB (66.56%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+ var expect = function (val){
+    obj= {
+        toBe(val1){
+            if (val === val1) return true
+            else throw new Error ("Not Equal")
+        },
+        notToBe(val1){
+            if(val !== val1) return true;
+            else throw new Error ("Equal")
+        }
+
+    }
+    return obj
+ };
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
