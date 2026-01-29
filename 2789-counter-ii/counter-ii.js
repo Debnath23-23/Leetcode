489d76dcf720862156d98a2c0dc1b7728c50e813
Debnath patra <150770621+Debnath23-23@users.noreply.github.com>
2026-01-29 20:57:44 +0530

    Time: 47 ms (75.17%) | Memory: 57.7 MB (20.10%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const n= init;
+
+var obj={
+     increment(){
+        return init+=1;
+    },
+     decrement(){
+        return init-=1;
+    },
+      reset(){
+        return init=n;
+    }
+}
+return obj;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
